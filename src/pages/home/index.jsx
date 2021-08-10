ac3d445c3d3ae9a22e7189283d0786f38b91da93
jihan <2529699061@qq.com>
2021-08-10 11:12:19 +0800

    add copy

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
-import { View, Text, CoverView, CoverImage, Image, ScrollView,  } from '@tarojs/components'
-import { Icon, List, Button, WingBlank, Card, Clipboard } from '@ant-design/react-native'
-// import { Clipboard } from 'react-native'
+import { View, Text, CoverView, CoverImage, Image, ScrollView } from '@tarojs/components'
+import { Icon, List, Button, WingBlank, Card, Toast } from '@ant-design/react-native'
+import Clipboard from '@react-native-clipboard/clipboard';
 import { connect } from 'react-redux'
 import adImg from '../../images/ad.png'
 import headImg from '../../images/1.png'
@@ -10,14 +10,25 @@ import './index.less'
 const Item = List.Item;
 class Home extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      imgArray: []
+    },
+    this.copyYqm = this.copyYqm.bind(this)
+  }
+
   UNSAFE_componentWillMount () { }
 
   componentDidMount () {
-    // console.log('one', this.props)
+    let imgArray = []
+    for(let i = 0; i <= 20; i++) {
+      imgArray.push({ id: i })
+    }
+    this.setState({ imgArray })
     this.props.dispatch({ 
       type: 'home/saveData',
     })
-    // Clipboard.setString('Hello World');
     // console.log('two', this.props.loading.effects['home/saveData'])
 
    }
@@ -28,7 +39,14 @@ class Home extends Component {
 
   componentDidHide () { }
 
+  copyYqm() {
+    Clipboard.setString('hhhhhhhh')
+    Toast.success('复制成功!!!', 1);
+  }
+
   render () {
+    const {imgArray} = this.state
+
     return (
       <ScrollView
         style={{ flex: 1, backgroundColor: '#f5f5f9' }}
@@ -43,25 +61,20 @@ class Home extends Component {
             <Text className='imgOnText'>去去去去去去去</Text>
             <Text className='imgOnTwoText'>西安市 0岁</Text>
             <View className='imgArray'>
-              <View style={{width: 50, height: 50, marginLeft: 10}} className='selectImgArrayOneImg'>
-                <Image
-                  style={{width: '100%', height: '100%', borderRadius: 10}}
-                  src={headImg}
-                />
-              </View>
-             
-              <View style={{width: 50, height: 50, marginLeft: 10}} className=''>
-                <Image
-                  style={{width: '100%', height: '100%', borderRadius: 10}}
-                  src={headImg}
-                />
-              </View>
-              <View style={{width: 50, height: 50, marginLeft: 10}}>
-                <Image
-                  style={{width: '100%', height: '100%', borderRadius: 10}}
-                  src={headImg}
-                />
-              </View>
+              <ScrollView
+                scrollX
+              >
+                {imgArray.map(reward => {
+                  return (
+                    <View style={{width: 50, height: 50, marginLeft: 10}} className='selectImgArrayOneImg' key={reward.id}>
+                      <Image
+                        style={{width: '100%', height: '100%', borderRadius: 10}}
+                        src={headImg}
+                      />
+                    </View>
+                  )
+                })}
+              </ScrollView>
             </View>
             <View className='bottomText'>
               <Icon name='alert' size='md' color='#efb336' />
@@ -128,7 +141,7 @@ class Home extends Component {
             extra={
               <View className='copyExtra' style={{ position: 'relative' }}>
                 <Text style={{position: 'absolute', bottom: -8, left: 30}}>aygl</Text>
-                <Button style={{position: 'absolute', right: 15, bottom: -16}} type='primary' className='copyButton'>复制</Button>
+                <Button style={{position: 'absolute', right: 15, bottom: -16}} type='primary' className='copyButton' onPress={this.copyYqm}>复制</Button>
               </View>
             }
           >
